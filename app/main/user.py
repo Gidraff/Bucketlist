a6dcc23dbd0b085c2ae2a3520adbfddca4274f59
Gidraff <kamandegid@gmail.com>
2017-07-20 18:12:30 +0300

    Removde name attribute from class

--- app/main/user.py
+++ app/main/user.py
@@ -2,17 +2,16 @@
 
 from datetime import datetime
 from uuid import uuid4
-from bucketlist import BucketList
-from data import Data
+from app.main.bucketlist import BucketList
+from app.main.data import Data
 
 class User(object):
     """User class that allows user to create, view, update and delete bucketlist"""
-    def __init__(self,name, user_name, email,password, user_id=None):
-        self.name = name
+    def __init__(self, user_name, email,password):
         self.user_name = user_name
         self.email = email
         self.password = password
-        self.user_id = uuid4().hex
+        self._id = uuid4().hex
 
     def create_bucketlist(self, title, description, date_created=datetime.utcnow()):
         """creates and saves user's bucketlist"""
@@ -36,40 +35,31 @@ class User(object):
         """Allows user to make update or edit buckelist"""
 
         for bucketlist in Data.bucketlists:
-            if bucketlist['owner_id'] == self.user_id:
+            if bucketlist['owner_id'] == self._id:
                 bucketlist['title'] = title
                 bucketlist['description'] = description
                 return True
+            else:
+                return False
 
     @staticmethod
     def delete_bucketlist(bucketlist_id):
-        """delete bucketlist with the  passed arguments"""
+    def user_detail_to_dict(self):
+        """return user details as a dictionary"""
 
-        for bucketlist in Data.bucketlists:
-            if bucketlist_id == bucketlist['bucketlist_id']:
-                del bucketlist
-                return True
+        return {
+                'user_name':self.user_name,
+                'email':self.email,
+                'password':self.password,
+                'user_id':self._id
+                }
+
     @staticmethod
     def verify_user(email):
         """checks whether user exists in the Data class"""
         email_checker = [i['email'] for i in Data.user if email == i['email']]
         return "".join(email_checker) == email
     
-    def user_detail_to_dict(self):
-        """return user details as a dictionary"""
-
-        return {
-                'name':self.name,
-                'user_name':self.user_name,
-                'email':self.email,
-                'password':self.password,
-                'user_id':self.user_id
-                }        
-
-    def save_info(self):
-        """save user's detail in Data.users"""
-        Data.user.append(self.user_detail_to_dict())
-
     @classmethod
     def register_user(cls, name, user_name, email, password):
         does_exist = cls.verify_user(email)
@@ -79,12 +69,10 @@ class User(object):
             return True
         else:
             return False
- 
 
-user_one = User
-p = user_one.register_user('raff', 'raff','raff@mail', '54321')
-print(Data.user)
-print(p)
-p1 = user_one.register_user('raff', 'raff','raff@mail', '54321')
-print(p1)
-print(Data.user)
+    def save_info(self):
+        """save user's detail in Data.users"""
+        Data.user.append(self.user_detail_to_dict())
+
+
+
