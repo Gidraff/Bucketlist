5786dfcb7479751c7e04e2754f7e6a9ed629a99e
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-08-25 21:04:54 +0300

    Add docstring for Set up

--- test/tests.py
+++ test/tests.py
@@ -5,67 +5,57 @@ from app.main.activity import Activity
 from app.main.bucketlist import BucketList
 from app.main.user import User
 
-class TestActivity(TestCase):
-    """Activity class Tests"""
-
-    def setUp(self):
-        self.activity = Activity('surfing')
-        
-    def test_is_instance_of_class_activity(self):
-        self.assertTrue(isinstance(self.activity, Activity))
-
-    def test_argument_given_returned(self):
-        self.assertEqual(self.activity.activity, 'surfing', msg='There is match')
-
-
 class TestBucketList(TestCase):
     """Main BucketList Test class"""
     def setUp(self):
-        """this prepares test fixture"""
-
+        """Sets up test fixture"""
         self.new_bucketlist = BucketList('bucket_title', 'am a bucket')
     def test_add_activity(self):
-        """tests whether the activity has been saved"""
+        """Test whether an activity has been saved"""
+        before_add = len(self.new_bucketlist.activities)
         self.new_bucketlist.add_activity('this is awesome')
-        self.assertEqual(len(self.new_bucketlist.activities), 1)
+        after_add = len(self.new_bucketlist.activities)
+        self.assertEqual(after_add - before_add, 1)
 
     def test_edit_activity(self):
-        "tests wether existing activity has been edited"
+        "Test whether  an activity has been edited"
         id = self.new_bucketlist.add_activity('this is cool')
         self.new_bucketlist.edit_acivity(id, 'this is awesome')
         self.assertEqual(self.new_bucketlist.activities[id].activity, 'this is awesome')
+
+    def test_delete_activity(self):
+        """Test whether an activity has been deleted"""
+        id = self.new_bucketlist.add_activity('this is an activity')
+        size_before = len(self.new_bucketlist.activities)
+        self.new_bucketlist.delete_activity(id)
+        size_after = len(self.new_bucketlist.activities)
+        self.assertTrue(size_before - size_after, 1)
+        
             
-
-
 class TestUser(TestCase):
-    """setUp fixture for tests"""
+    """Sets up fixture for tests"""
     def setUp(self):
+        """Set up test fixture"""
         self.new_user = User('mary', 'mary@mail', 'i1234')
 
     def test_create_bucketlist(self):
-        """Tests whether bucketlist has been  created"""
+        """Test whether bucketlist has been  created"""
         result = len(self.new_user.bucketlists)
         self.new_user.create_bucketlist('this is new', 'new bucketlist')
         after_add = len(self.new_user.bucketlists)
-        self.assertTrue(after_add > result)
+        self.assertEqual(after_add - result, 1)
 
     def test_update_bucketlist(self):
-        """test wether a bucketlist has been edited"""
+        """Test whether bucketlist has been edited"""
         id = self.new_user.create_bucketlist('this is another', 'yet another')
         self.new_user.update_bucketlist(id, 'this work', 'yes it does')
         self.assertEqual(self.new_user.bucketlists[id].title, 'this work')
         self.assertEqual(self.new_user.bucketlists[id].description, 'yes it does')
         
     def test_delete_bucketlist(self):
-        """test wether a bucketlist has been deleted"""
+        """Test whether  bucketlist has been deleted"""
         id = self.new_user.create_bucketlist('another one', 'bungee')
         result = len(self.new_user.bucketlists)
         self.new_user.delete_bucketlist(id)
-        self.assertNotEqual(len(self.new_user.bucketlists), result)
-
-
-        
-
-
-
-    
+        after_delete = len(self.new_user.bucketlists)
+        self.assertTrue(result, after_delete)
